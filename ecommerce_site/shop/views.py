f44c1194a9c1329f211e725be9ebd48814ecb420
Carlos Amaya <carlosamaya@Carloss-MacBook-Air.local>
2023-06-09 19:09:51 -0500

    Removed my_site directory

--- ecommerce_site/shop/views.py
+++ ecommerce_site/shop/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from .models import Products
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def index(request):
+    print("Request!")
     product_objects = Products.objects.all()
 
     item_name = request.GET.get('item_name')
 
     if (item_name != '' and item_name is not None):
         product_objects = product_objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_objects, 4)
+    print(request.GET)
+    page = request.GET.get('page')
+    print("paginator")
+    print(page)
+    product_objects = paginator.get_page(page)
 
     return render(request, 'shop/index.html', {'product_objects': product_objects})
