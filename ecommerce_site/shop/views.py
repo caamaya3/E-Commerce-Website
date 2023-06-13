08cc23fa1cd0624415967952c0c1933486463aa8
Carlos Amaya <carlosamaya@Carloss-MacBook-Air.local>
2023-06-13 15:48:26 -0500

    Added feature that allows the user to click on an item and view the details of the item, including the discount price and a short description about the item

--- ecommerce_site/shop/views.py
+++ ecommerce_site/shop/views.py
@@ -5,7 +5,11 @@ from django.core.paginator import Paginator
 
 
 def index(request):
-    print("Request!")
+    print("Product objects")
+
+    for product in Products.objects.all():
+        dir(product)
+
     product_objects = Products.objects.all()
 
     item_name = request.GET.get('item_name')
@@ -13,10 +17,16 @@ def index(request):
     if (item_name != '' and item_name is not None):
         product_objects = product_objects.filter(title__icontains=item_name)
     paginator = Paginator(product_objects, 4)
-    print(request.GET)
+
     page = request.GET.get('page')
-    print("paginator")
-    print(page)
+
     product_objects = paginator.get_page(page)
 
     return render(request, 'shop/index.html', {'product_objects': product_objects})
+
+# When an item is clicked on, this function allows us to retrieve the details.
+
+
+def detail(request, id):
+    product_object = Products.objects.get(id=id)
+    return render(request, 'shop/detail.html', {'product_object': product_object})
